2feb845a7492a0e854add662e426ff0cbe4caae3
DuongThan <ngoxuanduong230896@gmail.com>
2017-02-08 19:07:09 +0700

    First commit
    Add React

---
+++ React/DemoReact/DemoReact/Jsx/HelloReact.jsx
@@ -0,0 +1,11 @@
+﻿var HelloReact23 = React.createClass({
+    render: function () {
+        return (
+            <h1> {this.props.name} </h1>
+            );
+    }
+});
+
+React.render(
+    <HelloReact23 name="Hello Duongthan"/>,document.getElementById('container')
+    );
